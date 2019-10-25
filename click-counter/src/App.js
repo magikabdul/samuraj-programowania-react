83405db4ab706c7e6bcfa512332aa890b37fa25d
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-25 16:01:39 +0200

    feat: added component

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
             count: prevState.count + 1,
             result: 0
          }));
-      } else {
+      } else if (type === "addiction") {
          this.setState(prevState => ({
             count: prevState.count + 1,
             result: prevState.result + number
@@ -28,16 +28,21 @@ class App extends React.Component {
    render() {
       return (
          <>
-            <button onClick={this.handleMathClick.bind(this, "substraction", 10)}>-10</button>
-            <button onClick={() => this.handleMathClick("substraction", 1)}>-1</button>
-            <button onClick={this.handleMathClick.bind(this, "reset")}>Reset</button>
-            <button onClick={() => this.handleMathClick()}>+1</button>
-            <button onClick={() => this.handleMathClick("adding", 10)}>+10</button>
+            <MathButton name="-10" number="10" type="substraction" click={this.handleMathClick} />
+            <MathButton name="-1" number="1" type="substraction" click={this.handleMathClick} />
+            <MathButton name="Reset" type="reset" click={this.handleMathClick} />
+            <MathButton name="1" number="1" type="addiction" click={this.handleMathClick} />
+            <MathButton name="10" number="10" type="addiction" click={this.handleMathClick} />
             <h1>Liczba kliknięć: {this.state.count}</h1>
             <h1>Wynik: {this.state.result}</h1>
          </>
       );
    }
 }
+
+const MathButton = props => {
+   const number = parseInt(props.number);
+   return <button onClick={() => props.click(props.type, number)}>{props.name}</button>;
+};
 
 export default App;
