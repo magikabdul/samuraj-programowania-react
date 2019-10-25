0b743575fba14566085fc780f8264f850a939b18
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-25 12:00:36 +0200

    feat: added app ui

--- btn-show-hide/src/App.js
+++ btn-show-hide/src/App.js
@@ -2,9 +2,26 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-   state = {};
+   state = {
+      messageIsActive: false
+   };
+
+   handleClick = () => {
+      this.setState({
+         messageIsActive: !this.state.messageIsActive
+      })
+   }
+
    render() {
-      return <></>;
+      const text =
+         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum consequuntur facere saepe, doloremque, porro enim consequatur magnam commodi quos sit quae natus mollitia nostrum reiciendis eum id quisquam dicta culpa?";
+
+      return (
+         <>
+            <button onClick={this.handleClick}>Pokaż</button>
+            <p>{text}</p>
+         </>
+      );
    }
 }
 
