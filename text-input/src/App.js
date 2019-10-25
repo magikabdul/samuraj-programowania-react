8bbeecc397d027c8b24fad292dc352408299cacb
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-25 09:36:23 +0200

    chore: update for input and reset btn

--- text-input/src/App.js
+++ text-input/src/App.js
@@ -2,14 +2,38 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-   state = {};
+   state = {
+      value: ""
+   };
+
+   // tutaj, albo należy skorzystać z bind przy wywoływaniu metody, ale należy
+   // użyć funkcji strzałkowej
+   // handleInputChange(e) {
+   //    console.log(e.target.value);
+   //    this.setState({
+   //       value: e.target.value
+   //    });
+   // }
+
+   handleInputChange = e => {
+      this.setState({
+         value: e.target.value
+      });
+   };
+
+   handleReset = () => {
+      this.setState({
+         value: ""
+      });
+   };
 
    render() {
       return (
          <>
-            <input type="text"></input>
-            <button>Reset</button>
-            <h1></h1>
+            {/* <input placeholder="wpisz..." onChange={this.handleInputChange.bind(this)} type="text"></input> */}
+            <input value={this.state.value} placeholder="wpisz..." onChange={this.handleInputChange} type="text"></input>
+            <button onClick={this.handleReset}>Reset</button>
+            <h1 className="title">{this.state.value.toUpperCase()}</h1>
          </>
       );
    }
