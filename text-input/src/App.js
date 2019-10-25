c858c9f319b9d11f208feaf3955a1bf758a4bf03
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-25 09:08:22 +0200

    chore: basic app state

---
+++ text-input/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+   state = {};
+
+   render() {
+      return (
+         <>
+            <input type="text"></input>
+            <button>Reset</button>
+            <h1></h1>
+         </>
+      );
+   }
+}
+
+export default App;
