0745cbb12888da35456b70cc256002d90240dea5
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-28 18:03:09 +0100

    chore: initial state

---
+++ check-box/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+   state = {};
+   render() {
+      return (
+         <>
+            <div>Hello</div>
+         </>
+      );
+   }
+}
+
+export default App;
