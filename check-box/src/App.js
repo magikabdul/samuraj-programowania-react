dd04f3ad32d381cd8fc630a92ff3a086fb87e64c
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-29 16:17:13 +0100

    feat: rendering dependent od checkbox state

--- check-box/src/App.js
+++ check-box/src/App.js
@@ -1,12 +1,38 @@
 import React from "react";
 import "./App.css";
 
+const PositiveMessage = () => {
+   return <p>Możesz obejrzeć film. Zapraszamy!</p>;
+};
+
+const NegativeMessage = () => {
+   return <p>Nie możesz obejrzeć tego filmu, jeżeli masz nimiej niż 16 lat.</p>;
+};
+
 class App extends React.Component {
-   state = {};
+   state = {
+      isConfirmed: false
+   };
+
+   handleCheckboxChange = () => {
+      this.setState({
+         isConfirmed: !this.state.isConfirmed
+      });
+   };
+
    render() {
       return (
          <>
-            <div>Hello</div>
+            <h1>Kup bilet na horror roku !</h1>
+            <input
+               type="checkbox"
+               name=""
+               id="age"
+               onChange={this.handleCheckboxChange}
+               checked={this.state.isConfirmed}
+            />
+            <label htmlFor="age">Mam conajmniej 16 lat</label>
+            {this.state.isConfirmed ? <PositiveMessage /> : <NegativeMessage />}
          </>
       );
    }
