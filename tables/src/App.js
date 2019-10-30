d1969f67d0f506bb270be9d376408a22fb70142a
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-30 17:01:14 +0100

    feat: added buttons for filtering

--- tables/src/App.js
+++ tables/src/App.js
@@ -1,29 +1,101 @@
 import React from "react";
 import "./App.css";
 
-const Item = props => {
-   const { item } = props;
-
-   return <li>{`owoc ${item}`}</li>;
+const data = {
+   users: [
+      {
+         id: 1,
+         age: 29,
+         name: "Arek",
+         sex: "male"
+      },
+      {
+         id: 2,
+         age: 49,
+         name: "Marta",
+         sex: "female"
+      },
+      {
+         id: 3,
+         age: 19,
+         name: "Stasia",
+         sex: "female"
+      },
+      {
+         id: 4,
+         age: 24,
+         name: "Karol",
+         sex: "male"
+      }
+   ]
 };
 
-class ListItems extends React.Component {
+// const Buttons = props => {
+//    return (
+//       <>
+//          <button onClick={props.handleFilterChange("male")}>
+//             Pokaż wszysktkich
+//          </button>
+//          <button onClick={props.handleFilterChange}>Pokaż kobiety</button>
+//          <button onClick={props.handleFilterChange}>Pokaż mężczyzn</button>
+//       </>
+//    );
+// };
+
+const User = ({ user }) => {
+   return (
+      <div className="userInfo">
+         <h1>{user.name}</h1>
+         <p>Informacje o użytkowniku</p>
+         <p>
+            Wiek użytkownika: <strong>{user.age}</strong> lat
+         </p>
+         <p>Płeć użytkownika: {user.sex}</p>
+      </div>
+   );
+};
+
+class ListUsers extends React.Component {
    state = {
-      items: ["jabłko", "śliwka", "gruszka"]
+      filter: "all"
+   };
+
+   userList = () => {
+      let users = data.users;
+
+      if (this.state.filter === "male") {
+         users = users.filter(user => user.sex === "male");
+      } else if (this.state.filter === "female") {
+         users = users.filter(user => user.sex === "female");
+      }
+      users = users.map(user => <User user={user} key={user.id} />);
+
+      return users;
+   };
+
+   handleFilterChange = filter => {
+      this.setState({
+         filter: filter
+      });
    };
 
    render() {
       return (
          <>
-            <ul>
-               {this.state.items.map((item, idx) => {
-                  // tutaj przekazujemy key!!!!
-                  return <Item item={item} key={idx} />;
-               })}
-            </ul>
+            {/* <Buttons filter={this.handleFilterChange(this.state.filter)} /> */}
+            <button onClick={this.handleFilterChange.bind(this, "all")}>
+               Pokaż wszysktkich
+            </button>
+            <button onClick={this.handleFilterChange.bind(this, "female")}>
+               Pokaż kobiety
+            </button>
+            <button onClick={this.handleFilterChange.bind(this, "male")}>
+               Pokaż mężczyzn
+            </button>
+            {this.userList()}
          </>
       );
    }
 }
 
-export default ListItems;
+export default ListUsers;
