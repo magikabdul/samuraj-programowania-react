3cbbaf4ffaa10506402f01d4d52167c4cec03daa
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-30 13:53:04 +0100

    chore: destructing

--- basket/src/App.js
+++ basket/src/App.js
@@ -25,26 +25,28 @@ class App extends React.Component {
    };
 
    render() {
+      const { shoppingCart, availableProducts } = this.state;
+
       return (
          <div>
             <button
-               disabled={this.state.shoppingCart === 0 ? true : false}
+               disabled={shoppingCart === 0 ? true : false}
                onClick={this.handleRemoveFromCart}>
                -
             </button>
-            <span> {this.state.shoppingCart} </span>
+
+            <span
+               style={shoppingCart === 0 ? { opacity: 0.3 } : { opacity: 1 }}>
+               {" "}
+               {shoppingCart}{" "}
+            </span>
+
             <button
-               disabled={
-                  this.state.shoppingCart >= this.state.availableProducts
-                     ? true
-                     : false
-               }
+               disabled={shoppingCart >= availableProducts ? true : false}
                onClick={this.handleAddToCart}>
                +
             </button>
-            {this.state.shoppingCart > 0 && (
-               <button onClick={this.handleBuy}>Kup</button>
-            )}
+            {shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
          </div>
       );
    }
