96be32b98451dc06f39b087a11054b56c09625a4
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-30 13:08:17 +0100

    feat: btns add and remove

--- basket/src/App.js
+++ basket/src/App.js
@@ -2,9 +2,41 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-   state = {};
+   state = {
+      availableProducts: 7,
+      shoppingCart: 0,
+      canBuy: false
+   };
+
+   handleRemoveFromCart = () => {
+      this.setState({ shoppingCart: this.state.shoppingCart - 1 });
+   };
+
+   handleAddToCart = () => {
+      this.setState({ shoppingCart: this.state.shoppingCart + 1 });
+   };
+
    render() {
-      return <>Hello</>;
+      return (
+         <div>
+            <button
+               disabled={this.state.shoppingCart === 0 ? true : false}
+               onClick={this.handleRemoveFromCart}>
+               -
+            </button>
+            <span> {this.state.shoppingCart} </span>
+            <button
+               disabled={
+                  this.state.shoppingCart >= this.state.availableProducts
+                     ? true
+                     : false
+               }
+               onClick={this.handleAddToCart}>
+               +
+            </button>
+            <button>Kup</button>
+         </div>
+      );
    }
 }
 
