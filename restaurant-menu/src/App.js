b592cc431975f0a18f684340a1af4a118a54a22f
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-30 17:47:14 +0100

    chore: added Header and ListItems

--- restaurant-menu/src/App.js
+++ restaurant-menu/src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header.js";
+import ListItems from "./ListItems.js";
 
 class App extends React.Component {
-   state = {};
+   state = {
+      items: [
+         { id: 1, name: "herbata", active: true },
+         { id: 2, name: "zieminiaki", active: false },
+         { id: 3, name: "kasza", active: false },
+         { id: 4, name: "zupa wodna", active: false },
+         { id: 5, name: "wrzątek", active: false },
+         { id: 6, name: "chleb", active: true }
+      ]
+   };
+
    render() {
-      return <>Hallo</>;
+      return (
+         <>
+            <Header items={this.state.items} />
+            <ListItems />
+         </>
+      );
    }
 }
 
---
+++ restaurant-menu/src/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = props => {
+   return <header>Posumowanie zamówienia</header>;
+};
+
+export default Header;
