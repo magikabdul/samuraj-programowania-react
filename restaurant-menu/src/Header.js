4d42828259bae7fe9a8e7f20556f526a634fc611
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-05 09:09:40 +0100

    feat: order info displaying in Header

--- restaurant-menu/src/Header.js
+++ restaurant-menu/src/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Header = props => {
-   return <header>Podsumowanie zamówienia</header>;
+   const activeItems = props.items.filter(item => item.active);
+   const number = activeItems.length;
+
+   return (
+      <header>
+         <h2>Wielkość zamówienia: {number}</h2>
+         {number ? (
+            <h2>Do zapłaty: {number * 10} złotych</h2>
+         ) : (
+            <h2>Nie wybrałeś żadnych produktów</h2>
+         )}
+      </header>
+   );
 };
 
 export default Header;
