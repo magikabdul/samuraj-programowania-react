cbed9dd6265396c500534d1691f5f62a53f699f1
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-06 17:38:20 +0100

    feat: random divination

--- divination/src/components/App.js
+++ divination/src/components/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from "react";
+import Divination from "./Divination";
 
 class App extends Component {
    state = {
       divination: [
-         {id:1, text:"pierwsza"},
-         {id:2, text:"druga"},
-         {id:3, text:"trzecia"}
-      ]
+         { id: 1, text: "pierwsza" },
+         { id: 2, text: "druga" },
+         { id: 3, text: "trzecia" }
+      ],
+      randomText: ""
    };
+
+   handleRandomDivinationClick = () => {
+      const range = this.state.divination.length;
+      const idx = Math.floor(Math.random() * range);
+      // console.log(Math.floor(Math.random() * range));
+      this.setState({
+         randomText: this.state.divination[idx].text
+      });
+   };
+
    render() {
       return (
          <>
-            <button>Zabacz wróźbę</button>
+            <button
+               onClick={this.handleRandomDivinationClick}
+               style={{ margin: 20 }}>
+               Zabacz wróźbę
+            </button>
             <br />
-            <input type="text" name="" id="" />
+            <input style={{ margin: 20 }} type="text" name="" id="" />
             <button>Dodaj wróźbę</button>
+            <Divination text={this.state.randomText} />
          </>
       );
    }
---
+++ divination/src/components/Divination.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Divination = props => {
+   return (
+      <>
+         <h1 style={{ margin: 20 }}>{props.text}</h1>
+      </>
+   );
+};
+
+export default Divination;
