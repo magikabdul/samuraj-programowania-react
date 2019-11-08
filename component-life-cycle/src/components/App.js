bb437ac8f679d62e7b24bd8a00a582f6cadddf94
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-08 13:18:46 +0100

    chore: component life cycle app initial state

---
+++ component-life-cycle/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+   state = {};
+   render() {
+      return <>hhh</>;
+   }
+}
+
+export default App;
