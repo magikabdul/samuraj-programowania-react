1145925f17613aec5ef2cda7c97ab59f0f9abe75
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-13 14:52:48 +0100

    chore: app initial state

---
+++ data-example/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="app">
+        <h1>Hi</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
