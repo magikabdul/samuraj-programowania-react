9a9b5014e48f8a9e8a1887ea5c99f74fcd55957f
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-14 18:23:01 +0100

    chore: app initial state

---
+++ form-with-validation/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <div className="App">Form</div>;
+  }
+}
+
+export default App;
