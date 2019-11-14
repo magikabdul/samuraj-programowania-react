301f394023c3053b38a25030dce9f85152b2bf02
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-14 16:17:38 +0100

    feat: display 5 random users from API

--- fetch-practice/src/components/App.js
+++ fetch-practice/src/components/App.js
@@ -4,13 +4,35 @@ import "./App.css";
 import ButtonFetchUsers from "./ButtonFetchUsers";
 import UsersList from "./UsersList";
 
+const API = "https://randomuser.me/api/?results=5";
+
 class App extends Component {
-  state = {};
+  state = {
+    users: null
+  };
+
+  handleDataFetch = () => {
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response;
+        }
+        throw Error("Błąd !!!!");
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ users: data.results });
+      })
+      .catch(e => console.log(e));
+  };
+
   render() {
+    const users = this.state.users;
+
     return (
       <>
-        <ButtonFetchUsers />
-        <UsersList />
+        <ButtonFetchUsers click={this.handleDataFetch} />
+        {users ? <UsersList users={users} /> : users}
       </>
     );
   }
--- fetch-practice/src/components/UsersList.js
+++ fetch-practice/src/components/UsersList.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 const UsersList = props => {
-  return (
-    <ul>
-      <li>User</li>
-    </ul>
-  );
+  const users = props.users.map(user => (
+    <div key={user.login.uuid}>
+      <h4>{`${user.name.title} ${user.name.last}`}</h4>
+      <p>{user.email}</p>
+    </div>
+  ));
+
+  return <ul>{users}</ul>;
 };
 
 export default UsersList;
