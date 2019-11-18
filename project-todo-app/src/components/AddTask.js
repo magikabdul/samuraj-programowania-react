43aa538ae3689858b4229db86d5239a5460c5937
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-18 16:29:40 +0100

    feat: data in app and thier rendering

---
+++ project-todo-app/src/components/AddTask.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class AddTask extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="form">
+        addTask
+        <form action=""></form>
+      </div>
+    );
+  }
+}
+
+export default AddTask;
--- project-todo-app/src/components/App.js
+++ project-todo-app/src/components/App.js
@@ -1,14 +1,59 @@
 import React, { Component } from "react";
 
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
+
 import "./App.scss";
 
 class App extends Component {
-  state = {};
+  state = {
+    tasks: [
+      {
+        id: 0,
+        text: "zagrać",
+        date: "2019-11-18",
+        important: true,
+        active: true,
+        finishDate: null
+      },
+      {
+        id: 1,
+        text: "zrobić dobry uczynek",
+        date: "2019-01-08",
+        important: false,
+        active: true,
+        finishDate: null
+      },
+      {
+        id: 2,
+        text: "pomalować",
+        date: "2020-01-02",
+        important: true,
+        active: true,
+        finishDate: null
+      }
+    ]
+  };
+
+  deleteTask = id => {
+    console.log(id)
+  };
+
+  changeTaskStatus = id => {
+    
+  };
 
   render() {
     return (
       <>
-        <div>hello</div>
+        <div className="App">
+          <AddTask />
+          <TaskList
+            tasks={this.state.tasks}
+            delete={this.deleteTask}
+            change={this.changeTaskStatus}
+          />
+        </div>
       </>
     );
   }
