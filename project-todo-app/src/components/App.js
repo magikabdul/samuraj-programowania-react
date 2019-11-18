ac6d295226ea85c26975d62a0acd469808987a03
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-18 13:20:55 +0100

    chore: initial app state

---
+++ project-todo-app/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./App.scss";
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <div>hello</div>
+      </>
+    );
+  }
+}
+
+export default App;
