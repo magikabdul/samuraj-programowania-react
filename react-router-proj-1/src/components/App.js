85e1133c01bea7014ea58c266808914e545217b8
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-21 11:29:39 +0100

    feat: Switch usage for NavLink

--- react-router-proj-1/src/components/App.js
+++ react-router-proj-1/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import { BrowserRouter, NavLink, Route } from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
 
 const Home = () => <h1>Strona startowa</h1>;
 const News = () => <h1>Aktualności</h1>;
 const Contact = () => <h1>Kontakt do nas</h1>;
+
+const ErrorPage = () => <h1>Strona nie istnieje</h1>;
 
 class App extends Component {
   state = {};
@@ -38,9 +40,12 @@ class App extends Component {
             </nav>
           </header>
           <section>
-            <Route path="/" exact component={Home} />
-            <Route path="/news" component={News} />
-            <Route path="/contact" component={Contact} />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/news" component={News} />
+              <Route path="/contact" component={Contact} />
+              <Route component={ErrorPage} />
+            </Switch>
           </section>
         </>
       </BrowserRouter>
