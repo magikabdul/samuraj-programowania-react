6f7036d446751dc8e500c0081cb050f14a529dfa
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-22 13:58:43 +0100

    feat: header with fix images

--- react-router-proj-2/src/layouts/Header.js
+++ react-router-proj-2/src/layouts/Header.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import header1 from "../images/header1.jpg";
+import header2 from "../images/header2.jpg";
+import header3 from "../images/header3.jpg";
+
+import "../styles/Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact render={() => <img src={header1} alt="aaa" />} />
+        <Route path="/contact" render={() => <img src={header2} alt="bbb" />} />
+        <Route
+          path="/products"
+          render={() => <img src={header3} alt="ccc" />}
+        />
+        <Route path="/admin" render={() => <img src={header1} alt="ddd" />} />
+        <Route render={() => <img src={header3} />} />
+      </Switch>
+    </>
+  );
 };
 
 export default Header;
