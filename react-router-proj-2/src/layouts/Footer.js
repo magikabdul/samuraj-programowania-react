55e3f7ec50b3913bcc195fb184b1ecb1f74c8b6d
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-22 18:16:08 +0100

    feat: footer section

--- react-router-proj-2/src/layouts/Footer.js
+++ react-router-proj-2/src/layouts/Footer.js
@@ -1,7 +1,52 @@
 import React from "react";
 
+import "../styles/Footer.css";
+
+import { Route } from "react-router-dom";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h2>STOPKA</h2>
+      <Route
+        path="/"
+        exact
+        render={() => {
+          return (
+            <p>
+              Jesteś na <span>stronie głównej</span>
+            </p>
+          );
+        }}
+      />
+
+      <Route
+        path="/:page"
+        exact
+        render={props => {
+          return (
+            <p>
+              Jesteś na <span>{props.match.params.page}</span>
+            </p>
+          );
+        }}
+      />
+
+      <Route
+        path="/:page/:idProduct"
+        exact
+        render={props => {
+          return (
+            <p>
+              Jesteś na <span>{props.match.params.page}</span>
+              <br />
+              Jesteś na <span>{props.match.params.idProduct}</span>
+            </p>
+          );
+        }}
+      />
+    </div>
+  );
 };
 
 export default Footer;
