0eade65b027a5e986e2a7f4efc3918d0494443b6
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-22 17:46:19 +0100

    feat: product page

--- react-router-proj-2/src/layouts/Page.js
+++ react-router-proj-2/src/layouts/Page.js
@@ -5,8 +5,10 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ContactPage from "../pages/ContactPage";
 import ProductPage from "../pages/ProductPage";
+import ProductListPage from "../pages/ProductListPage";
 import AdminPage from "../pages/AdminPage";
 import ErrorPage from "../pages/ErrorPage";
+import LoginPage from "../pages/LoginPage";
 
 const Page = () => {
   return (
@@ -14,8 +16,10 @@ const Page = () => {
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/contact" component={ContactPage} />
-        <Route path="/products" component={ProductPage} />
+        <Route path="/product/:id" component={ProductPage} />
+        <Route path="/products" component={ProductListPage} />
         <Route path="/admin" component={AdminPage} />
+        <Route path="/login" component={LoginPage} />
         <Route component={ErrorPage} />
       </Switch>
     </>
