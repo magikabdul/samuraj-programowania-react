2c1038da11a213e046a845ddda6ad1d999ed71bc
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-23 10:58:21 +0100

    chore: css

--- styles-in-react/src/App.js
+++ styles-in-react/src/App.js
@@ -1,33 +1,16 @@
 import React, { Component } from "react";
 
+import Text from "./Text";
+
+import "./App.css";
+
 class App extends Component {
-  state = {
-    active: false
-  };
-
   render() {
-    let btn = {
-      border: "2px solid black",
-      padding: "10px 20px",
-      fontFamily: "arial",
-      fontSize: 30,
-      display: "block",
-      margin: "20px auto"
-    };
-
-    if (this.state.active) {
-      btn.backgroundColor = "cadetblue";
-      btn.color = "white";
-    }
-
     return (
-      <>
-        <button
-          style={btn}
-          onClick={() => this.setState({ active: !this.state.active })}>
-          {this.state.active ? "Wyłącz" : "Załącz"}
-        </button>
-      </>
+      <div className="app">
+        <h1>Witaj na stronie</h1>
+        <Text />
+      </div>
     );
   }
 }
---
+++ styles-in-react/src/Text.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Text = () => {
+  return (
+    <p className="red">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad saepe
+      voluptate reprehenderit explicabo? Id earum, maxime quas in illo cum
+      officia dolorum illum asperiores repellendus animi aspernatur, ea dolorem
+      veritatis!
+    </p>
+  );
+};
+
+export default Text;
