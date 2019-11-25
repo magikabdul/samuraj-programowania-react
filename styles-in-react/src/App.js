9821c68dd364f726aadae69ade5d1e139aa4abaf
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-11-25 11:02:14 +0100

    chore: dynamic add/remove classes

--- styles-in-react/src/App.js
+++ styles-in-react/src/App.js
@@ -5,10 +5,30 @@ import Text from "./Text";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    underline: false
+  };
+
   render() {
+    const text = "Witaj na stronie";
+    // let classes = "one";
+
+    // if (this.state.underline) classes += " line";
+    // if (text.length > 10) classes += " big";
+
+    const classes = ["one"];
+    if (this.state.underline) classes.push("line");
+    if (text.length > 10) classes.push("big");
+
     return (
-      <div className="app">
-        <h1>Witaj na stronie</h1>
+      <div
+        className="app"
+        onClick={() =>
+          this.setState({
+            underline: !this.state.underline
+          })
+        }>
+        <h1 className={classes.join(" ")}>Witaj na stronie</h1>
         <Text />
       </div>
     );
